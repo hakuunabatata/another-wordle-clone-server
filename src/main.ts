63821a8bfa934372b20a473c8d78ed095f1ed2fe
hakuunabatata <emachadomartins1999@gmail.com>
2023-07-02 17:49:47 -0300

    create prisma tables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-const { APP_PORT } = process.env
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(APP_PORT)
+  await app.listen(3333)
 }
+
 bootstrap()
